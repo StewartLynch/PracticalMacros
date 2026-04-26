7759547ff097e4ace2bf5f7741776c85942258d5
Stewart Lynch <slynch@createchsol.com>
2026-04-25 22:04:41 -0700

    Updated case

--- Sources/PracticalMacrosMacros/PracticalMacrosMacro.swift
+++ Sources/PracticalMacrosMacros/PracticalMacrosMacro.swift
@@ -129,7 +129,7 @@ public struct CaseIdentifiableMacro: MemberMacro, ExtensionMacro {
             let caseName = caseElement.name.text
             return
                 """
-                case .\(caseName)
+                case .\(caseName):
                     "\(caseName)"
                 """
         }.joined(separator: "\n")
